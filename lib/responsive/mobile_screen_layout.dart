220fb6675862c2007efd6cb28f0d359cd9c2f0be
Salomon Romano <salo_romano@yahoo.com>
2022-07-20 13:55:14 -0500

    User Provider

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:instagramclone/models/user.dart' as model;
+import 'package:instagramclone/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    model.User user = Provider.of<UserProvider>(context).getUser;
+
+    return Scaffold(
       body: Center(
-        child: Text('This is mobile'),
+        child: Text(user.username),
       ),
     );
   }
--- lib/responsive/responsive_layout_screen.dart
+++ lib/responsive/responsive_layout_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:instagramclone/providers/user_provider.dart';
 import 'package:instagramclone/utils/dimensions.dart';
+import 'package:provider/provider.dart';
 
-class ResponsiveLayout extends StatelessWidget {
+class ResponsiveLayout extends StatefulWidget {
   final Widget webScreenLayot;
   final Widget mobileScreenLayout;
 
@@ -12,13 +14,29 @@ class ResponsiveLayout extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+  @override
+  void initState() {
+    super.initState();
+    addData();
+  }
+
+  addData() async {
+    UserProvider userProvider = Provider.of(context, listen: false);
+    await userProvider.refreshUser();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
         if (constraints.maxWidth > webScreenSize) {
-          return webScreenLayot;
+          return widget.webScreenLayot;
         }
-        return mobileScreenLayout;
+        return widget.mobileScreenLayout;
       },
     );
   }
